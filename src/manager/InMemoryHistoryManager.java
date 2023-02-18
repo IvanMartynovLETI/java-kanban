e7c9987700d10fc732fbe12ff171284c720b196b
Ivan_M <iamartynov@rambler.ru>
2023-02-18 13:59:03 +0300

    fix: реализовать класс CustomLinkedList

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -8,94 +8,94 @@ import java.util.List;
 import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    @SuppressWarnings({"uncheked", "rawtypes"})
-    private final Map<Integer, Node> history = new HashMap<>();
-    @SuppressWarnings({"uncheked", "rawtypes"})
-    private Node head;
-    @SuppressWarnings({"uncheked", "rawtypes"})
-    private Node tail;
+    CustomLinkedList<Task> customLinkedList = new CustomLinkedList<>();
 
-    private static class Node<T extends Task> {
-        private final T task;
-        private Node<T> prev;
-        private Node<T> next;
+    @SuppressWarnings({"uncheked", "rawtypes"})
+    public class CustomLinkedList<T extends Task> {
 
-        protected Node(T task) {
-            this.task = task;
-            this.prev = null;
-            this.next = null;
+        public class Node<E extends Task> {
+            private final E task;
+            private Node<E> prev;
+            private Node<E> next;
+
+            public Node(E task) {
+                this.task = task;
+                this.prev = null;
+                this.next = null;
+            }
         }
-    }
 
-    @SuppressWarnings({"uncheked", "rawtypes"})
-    public void linkLast(Node node) {
-        if ((head == null) && (tail == null)) {//изначально список пустой
-            head = node;
-        } else {
-            tail.next = node;
-            node.prev = tail;
+        private final Map<Integer, Node> history = new HashMap<>();
+        private Node head;
+        private Node tail;
+
+        public void linkLast(T task) {
+            Node newNode = new Node<>(task);
+            if ((head == null) && (tail == null)) {//изначально список пустой
+                head = newNode;
+            } else {
+                tail.next = newNode;
+                newNode.prev = tail;
+            }
+            tail = newNode;
+            history.put(newNode.task.getId(), newNode);
         }
-        tail = node;
-    }
 
-    public ArrayList<Task> getTasks() {
-        ArrayList<Task> listOfTasksFromHistory = new ArrayList<>();
-        @SuppressWarnings({"uncheked", "rawtypes"})
-        Node currentNode = head;
+        public ArrayList<Task> getTasks() {
+            ArrayList<Task> listOfTasksFromHistory = new ArrayList<>();
+            Node currentNode = head;
 
-        while (currentNode != null) {
-            listOfTasksFromHistory.add(currentNode.task);
-            currentNode = currentNode.next;
+            while (currentNode != null) {
+                listOfTasksFromHistory.add(currentNode.task);
+                currentNode = currentNode.next;
+            }
+            return listOfTasksFromHistory;
         }
-        return listOfTasksFromHistory;
-    }
 
-    @Override
-    public void add(Task task) {
-        @SuppressWarnings({"uncheked", "rawtypes"})
-        Node newNode = new Node<>(task);
-        removeNode(newNode);
-        linkLast(newNode);
-        history.put(task.getId(), newNode);
-    }
-
-    @Override
-    public void remove(int id) {
-        if (head != null) { //удаление имеет смысл, только когда есть, что удалять
-            if (history.containsKey(id)) { //удаляемый элемент должен существовать в history
-                @SuppressWarnings({"uncheked", "rawtypes"})
-                Node nodeFound = history.get(id);
-                if ((nodeFound.prev == null) && (nodeFound.next == null)) {//удаляемый элемент-единственный
-                    head = null;
-                    tail = null;
-                } else if (nodeFound.prev == null) {//удаляемый элемент-первый
-                    head = nodeFound.next;
-                    head.prev = null;
-                    nodeFound.next = null;
-                } else if (nodeFound.next == null) {//удаляемый элемент-последний
-                    tail = nodeFound.prev;
-                    tail.next = null;
-                    nodeFound.prev = null;
-                } else {//удаляемый элемент не первый и не последний
-                    nodeFound.prev.next = nodeFound.next;
-                    nodeFound.next.prev = nodeFound.prev;
-                    nodeFound.prev = null;
-                    nodeFound.next = null;
+        public void removeNode(Node node) {
+            if (history.containsKey(node.task.getId())) {
+                if (head != null) { //удаление имеет смысл, только когда есть, что удалять
+                    if (history.containsKey(node.task.getId())) { //удаляемый элемент должен существовать в history
+                        Node nodeFound = history.get(node.task.getId());
+                        if ((nodeFound.prev == null) && (nodeFound.next == null)) {//удаляемый элемент-единственный
+                            head = null;
+                            tail = null;
+                        } else if (nodeFound.prev == null) {//удаляемый элемент-первый
+                            head = nodeFound.next;
+                            head.prev = null;
+                            nodeFound.next = null;
+                        } else if (nodeFound.next == null) {//удаляемый элемент-последний
+                            tail = nodeFound.prev;
+                            tail.next = null;
+                            nodeFound.prev = null;
+                        } else {//удаляемый элемент не первый и не последний
+                            nodeFound.prev.next = nodeFound.next;
+                            nodeFound.next.prev = nodeFound.prev;
+                            nodeFound.prev = null;
+                            nodeFound.next = null;
+                        }
+                        history.remove(node.task.getId());
+                    }
                 }
-                history.remove(id);
             }
         }
     }
 
-    @SuppressWarnings({"uncheked", "rawtypes"})
-    public void removeNode(Node node) {
-        if (history.containsKey(node.task.getId())) {
-            remove(node.task.getId());
+    @Override
+    public void add(Task task) {
+        remove(task.getId());
+        customLinkedList.linkLast(task);
+    }
+
+    @Override
+    public void remove(int id) {
+        if (customLinkedList.history.containsKey(id)) {
+            customLinkedList.removeNode(customLinkedList.history.get(id));
         }
     }
 
     @Override
     public List<Task> getHistory() {
-        return getTasks();
+        return customLinkedList.getTasks();
     }
 }
